f1515dc5ac88b4efea16aa7972d761555f68c084
Sandhya <sandhya4395@gmail.com>
2023-08-03 12:09:58 +0530

    Another Pattern program

---
+++ Pattern3.java
@@ -0,0 +1,28 @@
+package pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int i, j;
+	        // outer loop to handle number of rows
+	        for (i =6; i >= 1; i--) {
+	            // inner loop to handle number of columns
+	            for (j = 1; j <= i; j++) {
+	                // printing column values upto the row
+	                // value.
+	                System.out.print(j + " ");
+	            }
+	  
+	            // print new line for each row
+	            System.out.println();
+	        }
+	    }
+	  
+	   
+	    {
+	        
+	}
+
+}
